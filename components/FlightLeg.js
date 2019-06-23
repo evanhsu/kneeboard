025c1b27bfba49b5c1a6f82b3a913cdad1bcff1b
Evan Hsu <evanhsu@gmail.com>
2019-06-23 09:47:58 -0600

    Added FlightLeg component stub

---
+++ components/FlightLeg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    Text,
+} from 'react-native';
+
+function FlightLeg() {
+    return (
+        <Text>FlightLeg</Text>
+    );
+}
+
+export default FlightLeg;
+
--- screens/MissionsScreen.js
+++ screens/MissionsScreen.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import FlightLeg from '../components/FlightLeg';
 import {
-  View,
-  StyleSheet,
-  ScrollView,
-  Text,
+    View,
+    StyleSheet,
+    ScrollView,
+    Text,
 } from "react-native";
 
+function FlightLegList() {
+    return (
+        <View>
+            <FlightLeg/>
+            <FlightLeg/>
+            <FlightLeg/>
+        </View>
+    );
+}
+
+
 export default function MissionsScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Text>
-          hello
-        </Text>
-      </ScrollView>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ScrollView>
+                <Text style={styles.title}>
+                    Missions
+                </Text>
+                <FlightLegList/>
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    title: {
+        textAlign: 'center',
+        flex: 1,
+        fontSize: 24,
+    },
 });
